6c04f8908a69a2d08cfdde088d82120234189dda
sumithemmadi <sumithemmadi244@gmail.com>
2021-10-15 12:12:59 +0530

    Updated

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-var argv = require('minimist')(process.argv.slice(2));
+// var argv = require('minimist')(process.argv.slice(2));
+// console.log(argv);
+
+const yargs = require('yargs');
+
+const argv = yargs
+    .command('lyr', 'Tells whether an year is leap year or not', {
+        year: {
+            description: 'the year to check for',
+            alias: 'y',
+            type: 'number',
+        }
+    })
+    .option('time', {
+        alias: 't',
+        description: 'Tell the present Time',
+        type: 'boolean',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+if (argv.time) {
+    console.log('The current time is: ', new Date().toLocaleTimeString());
+}
+
+if (argv._.includes('lyr')) {
+    const year = argv.year || new Date().getFullYear();
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        console.log(`${year} is a Leap Year`);
+    } else {
+        console.log(`${year} is NOT a Leap Year`);
+    }
+}
+
 console.log(argv);
