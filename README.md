e65e15dd00861de05fc1a3526662892a11c125ae
sumithemmadi <sumithemmadi244@gmail.com>
2021-10-15 12:53:36 +0530

    Updated

--- README.md
+++
@@ -1,1 +0,0 @@
-# truecaller-nodejs
--- index.js
+++ index.js
@@ -1,36 +1,41 @@
+#!/usr/bin/env node
+
 // var argv = require('minimist')(process.argv.slice(2));
 // console.log(argv);
-
+var util = require('util');
 const yargs = require('yargs');
 
 const argv = yargs
-    .command('lyr', 'Tells whether an year is leap year or not', {
-        year: {
-            description: 'the year to check for',
-            alias: 'y',
+//    .usage('Usage: $0 -w [num] -yh[num]')
+//    .demand(['w','h'])
+    .command('login', 'login to truecaller', {
+        number: {
+            description: 'number',
+            alias: 'n',
             type: 'number',
         }
     })
-    .option('time', {
-        alias: 't',
-        description: 'Tell the present Time',
-        type: 'boolean',
+    .option('search', {
+        alias: 's',
+        description: 'To search caller id of a number',
+        type: 'charecter',
     })
     .help()
     .alias('help', 'h')
     .argv;
 
-if (argv.time) {
-    console.log('The current time is: ', new Date().toLocaleTimeString());
+if (argv.search) {
+    console.log('The current time is: ', argv.s);
 }
 
-if (argv._.includes('lyr')) {
-    const year = argv.year || new Date().getFullYear();
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-        console.log(`${year} is a Leap Year`);
-    } else {
-        console.log(`${year} is NOT a Leap Year`);
-    }
+if (argv._.includes('login')) {
+   // const year = argv.year || new Date().getFullYear();
+   // if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+     //   console.log(`${year} is a Leap Year`);
+   // } else {
+     //   console.log(`${year} is NOT a Leap Year`);
+  //  }
+     console.log ("Login");
 }
 
 console.log(argv);
