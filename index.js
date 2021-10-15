91fea9d433efed1f7e0a58e0e618581d5a0e8a8f
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-10-15 19:32:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ var util = require('util');
 const yargs = require('yargs');
 var PhoneNumber = require( 'awesome-phonenumber' );
 
-
-function get_json_response(number) {
-    var pn = new PhoneNumber(number,'IN');
-    var data = JSON.stringify( pn, null, 4 );
-    return data;
-}
-
-
-
 const argv = yargs
-    .usage('Login : $0 login +919912345678\n  Usage : $0 -s +919912345678')
+//    .usage('Usage: $0 -w [num] -yh[num]')
+//    .demand(['w','h'])
     .command('login', '(login to truecaller)', {
         number: {
             description: 'number',
@@ -41,11 +33,11 @@ if (argv._[0] == 'login') {
 }
 
 if (argv.s) {
-    var pn = get_json_response(argv.s);
-    if (pn.valid) {
+    var pn = PhoneNumber( argv.s, 'IN' );
+    if (!pn.isValid( )) {
         console.log("!invalid number");
         return false;
     } else {
-        console.log(pn);
+        console.log(JSON.stringify( pn, null, 4 ));
     }
 }
