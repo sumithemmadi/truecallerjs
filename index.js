65a0851bf51bdaf3d1d2035c348f613c570a44ee
sumithemmadi <sumithemmadi244@gmail.com>
2021-10-17 19:00:15 +0530

    Updated

--- index.js
+++ index.js
@@ -62,6 +62,35 @@ function userLogin(number, regionCode, countryCode,internationalNumber) {
 }
 
 function otpVerification(number,regionCode,countryCode,RequestID,otp){
+  const postdata = {
+    countryCode: regionCode,
+    dialingCode: countryCode,
+    phoneNumber: number,
+    RequestID,
+    otp
+  }
+  const axiosInstance = axios.create({
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+      "accept-encoding": "gzip",
+      "user-agent": "Truecaller/11.75.5 (Android;10)",
+      clientsecret: "lvc22mp3l1sfv6ujg83rd17btt"
+    },
+  });
+  console.log(postdata);
+  return axiosInstance
+  .post(
+    "https://account-asia-south1.truecaller.com/v2/sendOnboardingOtp",
+    postdata
+  )
+  .then(
+    (response) => {
+      return response.data;
+    },
+    (err) => {
+      return err.response.data;
+    }
+  );
 
 }
 //search number function
