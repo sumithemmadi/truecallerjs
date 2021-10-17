c95a33fdbc567533d87f8869c15829926e830007
sumithemmadi <sumithemmadi244@gmail.com>
2021-10-17 15:36:03 +0530

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 const yargs = require("yargs");
 const PhoneNumber = require("awesome-phonenumber");
 const prompt = require("prompt-sync")();
-var mainFile = require("./src/main");
+const axios = require("axios").default;
+var randomstring = require("randomstring");
+
+// User login function
+function userLogin(number, regionCode, countryCode,internationalNumber) {
+  console.log("Sending OTP to",internationalNumber);
+  let postdata = {
+    countryCode: regionCode,
+    dialingCode: countryCode,
+    installationDetails: {
+      app: {
+        buildVersion: 5,
+        majorVersion: 11,
+        minorVersion: 7,
+        store: "GOOGLE_PLAY"
+      },
+      device: {
+        deviceId: "2fa2d0b0h82ef00",
+        language: "en",
+        manufacturer: "Xiaomi",
+        model: "M2010J19SG",
+        osName: "Android",
+        osVersion: "10",
+        mobileServices: [
+          "GMS"
+        ]
+      },
+      language: "en"
+    },
+    phoneNumber: number,
+    region: 'region-2',
+    sequenceNo: 2
+  }
+
+  const axiosInstance = axios.create({
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+      "accept-encoding": "gzip",
+      "user-agent": "Truecaller/11.75.5 (Android;10)",
+      clientsecret: "lvc22mp3l1sfv6ujg83rd17btt"
+    },
+  });
+
+  return axiosInstance
+  .post(
+    "https://account-asia-south1.truecaller.com/v2/sendOnboardingOtp",
+    postdata
+  )
+  .then(
+    (response) => {
+      return response.data;
+    },
+    (err) => {
+      return err.response.data;
+    }
+  );
+
+}
+
+//search number function
+function searchNumber(number, regionCode, countryCode, internationalNumber) {
+  return true;
+}
+
+function authenticationRequest(
+  number,
+  regionCode,
+  countryCode
+) {
+  
+}
+
 
 const argv = yargs
   .usage(
@@ -34,9 +105,12 @@ if (argv._.includes("login") && argv._[0] == "login" && argv._.length == 1) {
     let internationalNumber = pn.getNumber("e164");
     let countryCode = pn.getCountryCode();
 
-    userLogin(number, regionCode, countryCode, internationalNumber);
+    let sendOtp = userLogin(number = "9912896765", "in", 91,"+919912896765");
+    sendOtp.then(function (response) {
+      console.log(response);
+    })
   }
-  process.exit();
+  // process.exit();
 }
 
 if (argv.s) {
@@ -53,3 +127,4 @@ if (argv.s) {
     searchNumber(number, regionCode, countryCode, internationalNumber);
   }
 }
+
--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,8 @@ const axios = require("axios").default;
 
 // User login function
 function userLogin(number, regionCode, countryCode, internationalNumber) {
-  console.log("Sending OTP to ${internationalNumber} ")
+  console.log("Sending OTP to ${internationalNumber} ");
+
   let sendOtp = authenticationRequest(number = "9912896765", "in", 91);
   sendOtp.then(function (response) {
     console.log(response);
@@ -43,63 +44,5 @@ function getRandomNumber(length) {
   return Math.floor(
     Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1)
   );
-}
-
-function authenticationRequest(
-  number,
-  regionCode,
-  countryCode
-) {
-  let postdata = {
-    countryCode: regionCode,
-    dialingCode: countryCode,
-    installationDetails: {
-      app: {
-        buildVersion: 5,
-        majorVersion: 11,
-        minorVersion: 7,
-        store: "GOOGLE_PLAY"
-      },
-      device: {
-        deviceId: "2fa2d0b0h82ef00",
-        language: "en",
-        manufacturer: "Xiaomi",
-        model: "M2010J19SG",
-        osName: "Android",
-        osVersion: "10",
-        mobileServices: [
-          "GMS"
-        ]
-      },
-      language: "en"
-    },
-    phoneNumber: number,
-    region: 'region-2',
-    sequenceNo: 2
-  }
-
-
-  const axiosInstance = axios.create({
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-      "accept-encoding": "gzip",
-      "user-agent": "Truecaller/11.75.5 (Android;10)",
-      clientsecret: "lvc22mp3l1sfv6ujg83rd17btt"
-    },
-  });
-
-  return axiosInstance
-    .post(
-      "https://account-asia-south1.truecaller.com/v2/sendOnboardingOtp",
-      postdata
-    )
-    .then(
-      (response) => {
-        return response.data;
-      },
-      (err) => {
-        return err.response.data;
-      }
-    );
 }
 
