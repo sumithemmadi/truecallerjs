c477a7ff217d7bdb9e7746ed42cbb6cede3f08ea
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-10-22 14:49:05 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const truecallerjs = require("..");
 
+// truecallerjs.searchNumber("MOBILE_NUMBER", "DEFAULT_COUNTRY_CODE", "YOUR_TRUECALLER_INSTALLATION_ID")
+
 var sn = truecallerjs.searchNumber("9912857147", 'IN', "aaddsgjfyychvbjhcfftyhhyhgggg");
 sn.then(function(response) {
     console.log(response);
