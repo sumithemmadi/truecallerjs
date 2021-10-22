420a49bb4caaf1b34a168cfb07e735e4ee7b6640
sumithemmadi <sumithemmadi244@gmail.com>
2021-10-22 07:15:59 +0530

    Updated

--- test/test.js
+++ test/test.js
@@ -1,2 +1,6 @@
+const truecallerjs = require(".");
 
-
+var sn = truecallerjs.searchNumber("9912857147", 'IN', "aaddsgjfyychvbjhcfftyhhyhgggg");
+sn.then(function(response) {
+    console.log(response);
+});
