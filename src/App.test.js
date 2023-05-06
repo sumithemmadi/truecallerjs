3dec5269932964bab7694cbf48cb54398e3e6cf5
sumithemmadi <sumithemmadi244@gmail.com>
2023-05-07 01:27:56 +0530

    started

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
