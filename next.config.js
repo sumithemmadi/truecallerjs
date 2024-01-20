334bb427b87d74ebb07a6fde86fc184c08f1e91f
sumithemmadi <sumithemmadi244@gmail.com>
2024-01-20 10:05:17 +0530

    Update

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/truecallerjs" : "";
+
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
